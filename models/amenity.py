e25431566c6a0c8b5e87474a002f70d85f8603a9
Tshupane Morake <tshupane@outlook.com>
2023-08-23 20:12:30 +0200

    Revert "Fixing🛠️"
    This reverts commit 69dfc51f6dfac010945c8878b79fd42796873fa9.

--- models/amenity.py
+++ models/amenity.py
@@ -3,11 +3,8 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-
-
 class Amenity(BaseModel, Base):
-    """Class to represent an amenities in the database"""
-
+    """ Class to represent an amenities in the database """
     __tablename__ = "amentiy"
     name = Column(String(128), nullable=False)
     place_amenities = relationship("Place", secondary="place_amenity", backref="amenities")
--- models/place.py
+++ models/place.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
+from os import getenv
 import models
 from models.review import Review
-from models.amenity import Amenity
 from models.base_model import BaseModel
 from models.base_model import Base
 from sqlalchemy import Column, Float, ForeignKey
-from sqlalchemy import Integer, String, Table
+from sqlalchemy import Integer, String
 from sqlalchemy.orm import relationship
 
 
@@ -24,7 +24,6 @@ class Place(BaseModel, Base):
     price_by_night = Column(Integer, default=0, nullable=False)
     latitude = Column(Float)
     longitude = Column(Float)
-    amenity_ids = []
 
     # For DBStorage
     reviews = relationship("Review", backref="place", cascade="delete")
@@ -35,44 +34,24 @@ class Place(BaseModel, Base):
         Returns the list of Review instances with place_id equals to the current Place.id
         """
         return self.reviews.filter(Review.place_id == self.id)
-
-    place_amenity = Table(
-        "place_amenity",
-        Base.metadata,
-        Column(
-            "place_id",
-            String(60),
-            ForeignKey("places.id"),
-            primary_key=True,
-            nullable=False,
-        ),
-        Column(
-            "amenity_id",
-            String(60),
-            ForeignKey("amenities.id"),
-            primary_key=True,
-            nullable=False,
-        ),
-    )
-
-    amenities = relationship("Amenity", secondary=place_amenity, viewonly=False)
-
-    amenity_ids = []
-
+    
     def get_amenities(self):
         """
-        Returns the list of Amenity instances based on the attribute amenity_ids that contains
+        Returns the list of Amenity instances based on the attribute amenity_ids that contains 
         all Amenity.id linked to the Place
         """
         return [
-            models.storage.get(Amenity, amenity_id) for amenity_id in self.amenity_ids
+            models.Amenity.query.get(amenity_id)
+            for amenity_id in self.amenity_ids
         ]
 
     def amenities_set(self, amenities):
         """
-        Handle append method for adding an Amenity.id to the attribute amenity_ids. This method
+        Handle append method for adding an Amenity.id to the attribute amenity_ids. This method 
         should accept only Amenity object.
         """
         if not isinstance(amenities, list):
             return
         self.amenity_ids.extend([amenity.id for amenity in amenities])
+
+    place_amenities = relationship("Amenity", secondary="place_amenity", viewonly=False)
