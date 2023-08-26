dc95a898cea9dc1a9c5e70e241638f0e1d17531b
shield <myshield02@gmail.com>
2023-08-26 17:18:18 +0100

    fixing the create model

--- console.py
+++ console.py
@@ -156,44 +156,6 @@ class HBNBCommand(cmd.Cmd):
         else:
             print("** class name missing **")
 
-        # args_list = args.split()
-
-        # if not args_list:
-        #     print("** class name missing **")
-        #     return
-        # elif args_list[0] not in HBNBCommand.classes:
-        #     print("** class doesn't exist **")
-        #     return
-        
-        # class_name = args_list[0]
-        # new_instance = HBNBCommand.classes[class_name]()
-
-        # for attr in args_list[1:]:
-        #     key_param = attr.partition("=")
-        #     key_name = key_param[0]
-        #     key_value = key_param[2]
-        #     if key_value.startswith('"') and key_value.endswith('"'):
-        #         key_value = key_value[1:-1]
-        #         key_value = key_value.replace("_", " ")
-        #     elif "." in key_value:
-        #         try:
-        #             key_value = float(key_value)
-        #         except ValueError:
-        #             continue
-        #     else:
-        #          try:
-        #              key_value = int(key_value)
-        #          except ValueError:
-        #              continue
-
-        #     if hasattr(new_instance, key_name):
-        #         setattr(new_instance, key_name, key_value)
-
-        # storage.new(new_instance)
-        # storage.save()
-        # print(new_instance.id)
-        # storage.save()
-
     def help_create(self):
         """ Help information for the create method """
         print("Creates a class of any type")
