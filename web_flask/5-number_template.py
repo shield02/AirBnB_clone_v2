f1de16c6cb214a0abc1dd8018fd46ea360542ad0
shield <myshield02@gmail.com>
2023-09-23 19:35:39 +0100

    use template to display custom page with given number

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""application with flask framework"""
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_world():
+    """display a string"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def HBNB():
+    """display HBNB"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def text(text):
+    """use given text"""
+    return "C {}".format(text.replace("_", " "))
+
+
+@app.route('/python/', defaults={'text': 'is_cool'})
+@app.route('/python/<text>', strict_slashes=False)
+def display(text):
+    """display “Python ”, followed by the value of given text"""
+    return "Python {}".format(text.replace("_", " "))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def num_display(n):
+    """display “n is a number” only"""
+    return f"{n} is a number"
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def num_html(n):
+    """use templating to display "n" is a number only"""
+    return render_template('5-number.html', name=n)
+
+
+if __name__ == "__main__":
+    app.run()
