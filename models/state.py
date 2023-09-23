3d41b5d790c913a80f28ea1e03205d3f0a348719
shield <myshield02@gmail.com>
2023-09-23 21:04:44 +0100

    modified function to use DBStorage in the getter method

--- models/state.py
+++ models/state.py
@@ -12,11 +12,11 @@ from sqlalchemy.orm import relationship
 
 class State(BaseModel, Base):
     """ State class """
-    __tablename__ = "states"
-    name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state", cascade="delete")
-
-    if getenv("HBNB_TYPE_STORAGE") != "db":
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        __tablename__ = "states"
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state", cascade="delete")
+    else:
         @property
         def cities(self):
             """Get all related city objects"""
