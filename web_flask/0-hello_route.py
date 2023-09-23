2fa95655055ed585fa3eb3fbf645fd445cafdb62
shield <myshield02@gmail.com>
2023-09-23 18:20:25 +0100

    start a flask web application

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -4,12 +4,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
-
 @app.route("/", strict_slashes=False)
 def hello_world():
     """display a string"""
     return "Hello HBNB!"
-
 
 if __name__ == "__main__":
     app.run()
